77c2fa2c0cbf639629aed25b3390d573a7e1b3a8
rc0035391 <rc0035391@CRO-24-02>
2019-02-25 15:51:34 +0000

    Add project files.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            double pi = 3.141592653589793;
+            richTextBox1.AppendText(pi.ToString() + "\n");
+            richTextBox1.AppendText(pi.ToString("N") + "\n");
+            richTextBox1.AppendText(pi.ToString("N3") + "\n");
+            richTextBox1.AppendText(pi.ToString("N0") + "\n");
+            richTextBox1.AppendText(pi.ToString("0") + "\n");
+            richTextBox1.AppendText(pi.ToString("0.0") + "\n");
+            richTextBox1.AppendText(pi.ToString("0.0E0") + "\n");
+            richTextBox1.AppendText(pi.ToString("0.00E00") + "\n");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
